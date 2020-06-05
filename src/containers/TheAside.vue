e6d99a530ae8cae32733eb72a29646bc3da9f16e
linty <linty@lintyone.cn>
2020-06-05 22:23:16 +0800

    init commit

---
+++ src/containers/TheAside.vue
@@ -0,0 +1,26 @@
+<template>
+  <CSidebar 
+    aside 
+    :show.sync="show" 
+    colorScheme="light"
+    overlaid
+    size="lg"
+  >
+    <CSidebarClose @click.native="show= !show"/>
+  </CSidebar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'TheAside',
+  data () {
+    return {
+      show: false
+    }
+  },
+  mounted () {
+    this.$root.$on('toggle-aside', () => this.show = !this.show)
+  }
+})
+</script>
---
+++ src/containers/TheContainer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="c-app" :class="{ 'c-dark-theme': $store.state.darkMode }">
+    <TheSidebar/>
+    <CWrapper>
+      <TheHeader/>
+      <div class="c-body">
+        <main class="c-main">
+          <CContainer fluid>
+            <transition name="fade">
+              <router-view></router-view>
+            </transition>
+          </CContainer>
+        </main>
+        <TheFooter/>
+      </div>
+    </CWrapper>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import TheSidebar from './TheSidebar.vue'
+import TheHeader from './TheHeader.vue'
+import TheFooter from './TheFooter.vue'
+
+export default Vue.extend({
+  name: 'TheContainer',
+  components: {
+    TheSidebar,
+    TheHeader,
+    TheFooter
+  }
+})
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
