f62f79dce1b19ab1bbfd73f581fdd6700f69b5d9
linty <linty@lintyone.cn>
2020-06-15 14:53:10 +0800

    feat : 时间格式化

---
+++ src/common/DateUtil.ts
@@ -0,0 +1,6 @@
+export const formatDate = (dateString: string) => {
+    let date = new Date(Date.parse(dateString))
+
+    date.setTime(date.setHours(date.getHours() + (0 - new Date().getTimezoneOffset() / 60)))
+    return date.toLocaleDateString() + " " + date.toTimeString()
+}
